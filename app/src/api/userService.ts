a9c8afd88d33efbf9eeea2942c35528cf7704c50
Juan David Muñoz Pasquel <jmunozpasquel@gmail.com>
2025-06-14 15:50:56 -0500

    corrections

--- app/src/api/userService.ts
+++ app/src/api/userService.ts
@@ -15,17 +15,7 @@ export interface RegisterUserPayload {
 const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 export async function registerUser(payload: RegisterUserPayload) {
-  const response = await axios.post(`${API_URL}/api/users/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  });
+  const response = await axios.post(`${API_URL}/api/users/`, payload);
 
-  if (response.status !== 200) {
-    throw new Error("Error en la solicitud");
-  }
-
-  return await response.data();
+  return response.data;
 }
